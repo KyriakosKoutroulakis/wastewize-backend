30859a2e9561c499915f09ae1543725b542fa803
Kyriakos Koutroulakis <koutroulakisk@gmail.com>
2024-02-01 01:35:08 +0200

    Add CORS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const dotenv = require('dotenv').config()
 
 const { connectDatabse } = require('./configs/mongodbConf')
@@ -11,6 +12,7 @@ connectDatabse()
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
